3aafb560cbbd3ee4ebc9725909a9318d9908a416
Dmitriy Belyaev <belyaev.markov@yandex.ru>
2022-06-22 09:12:26 +0300

    commit-7

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,11 @@
-const background = document.querySelector('.background');
 const popup = document.querySelector('.popup');
 const form = document.querySelector('.popup__container');
 
 const editButton = document.querySelector('.profile__button-edit');
 const offButton = document.querySelector('.popup__button-off');
 
-const formFieldTop = document.querySelector('.popup__field_position_top');
-const formFieldBottom = document.querySelector('.popup__field_position_bottom');
+const formFieldTop = document.querySelector('.popup__field_type_profession');
+const formFieldBottom = document.querySelector('.popup__field_type_specialization');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 
@@ -15,13 +14,11 @@ const cardsButton = document.querySelectorAll('.card__button');
 function openForm() {
   formFieldTop.value = profileTitle.textContent;
   formFieldBottom.value = profileSubtitle.textContent;
-  background.classList.remove('background_closed');
-  popup.classList.remove('popup_closed');
+  popup.classList.add('popup_open');
 }
 
 function closeForm() {
-  background.classList.add('background_closed');
-  popup.classList.add('popup_closed');
+  popup.classList.remove('popup_open');
 }
 
 function submitForm(evt) {
@@ -41,6 +38,4 @@ for (let index = 0; index < cardsButton.length; index++) {
     element.classList.add('card__button_active');
   }
   );
-
-
 }
